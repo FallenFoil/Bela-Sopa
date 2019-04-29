bb5b754ba127d0f8791cb47a5d389efd6631c48e
CesarAugustoBorges <cesar.a.borges98@gmail.com>
2019-04-29 15:41:23 +0100

    Added classes for Assistente

---
+++ projeto/WebApplication1/WebApplication1/Models/Assistente/Ingrediente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Assistente
+{
+    public class Ingrediente
+    {
+        private string descricao;
+        private string nome;
+        private string imagePath;
+        private string link;
+    }
+}
---
+++ projeto/WebApplication1/WebApplication1/Models/Assistente/Receita.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models.Assistente;
+
+namespace WebApplication1.Models
+{
+    public class Receita
+    {
+        private string nome;
+        private string dificuldade;
+        private int tempo;
+        private string descricao;
+        private string video;
+        private List<String> etiquestas;
+        //valores nutricionais
+        private int nPessoas;
+        private string link;
+        private List<Processo> processos;
+        private Dictionary<Ingrediente, int> ingredientes;
+        private List<Utensilio> utensilio;
+        private List<Tecnica> tecnicas;
+    }
+}
