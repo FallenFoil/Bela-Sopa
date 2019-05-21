a4d7e4d6a0ad66431586d7ddc1384c96c6fac5da
Alberto Faria <albertofaria.dev@gmail.com>
2019-05-21 20:44:19 +0100

    Informative commit message.

--- projeto/Bela Sopa/App/App/Shared/RecursosEmbutidos.cs
+++ projeto/Bela Sopa/App/App/Shared/RecursosEmbutidos.cs
@@ -1,8 +1,11 @@
 using BelaSopa.Models.Assistente;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
+using YamlDotNet.RepresentationModel;
 
 namespace App.Shared
 {
@@ -10,42 +13,49 @@ namespace App.Shared
     {
         public static IEnumerable<Receita> CarregarReceitasDeExemplo()
         {
-            var nomesRecursos =
-                Assembly
-                .GetEntryAssembly()
-                .GetManifestResourceNames()
-                .Where(name => name.StartsWith("App.Data.Receitas."));
-
-            foreach (var nome in nomesRecursos)
+            return CarregarRecursos("App.Data.Receitas.", stream =>
             {
-                using (var stream = Assembly.GetEntryAssembly().GetManifestResourceStream(nome))
-                    yield return CarregarReceita(stream);
-            }
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    var yamlStream = new YamlStream();
+                    yamlStream.Load(reader);
+
+                    var root = yamlStream.Documents[0].RootNode;
+
+                    return new Receita();
+                }
+            });
         }
 
         public static IEnumerable<Ingrediente> CarregarIngredientesDeExemplo()
         {
+            return CarregarRecursos("App.Data.Ingredientes.", stream =>
+            {
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    var yamlStream = new YamlStream();
+                    yamlStream.Load(reader);
+
+                    var root = yamlStream.Documents[0].RootNode;
+
+                    return new Ingrediente();
+                }
+            });
+        }
+
+        private static IEnumerable<T> CarregarRecursos<T>(string prefixoNome, Func<Stream, T> carregador)
+        {
             var nomesRecursos =
                 Assembly
                 .GetEntryAssembly()
                 .GetManifestResourceNames()
-                .Where(name => name.StartsWith("App.Data.Ingredientes."));
+                .Where(name => name.StartsWith(prefixoNome));
 
             foreach (var nome in nomesRecursos)
             {
                 using (var stream = Assembly.GetEntryAssembly().GetManifestResourceStream(nome))
-                    yield return CarregarIngrediente(stream);
+                    yield return carregador(stream);
             }
-        }
-
-        private static Receita CarregarReceita(Stream stream)
-        {
-
-        }
-
-        private static Ingrediente CarregarIngrediente(Stream stream)
-        {
-
         }
     }
 }
