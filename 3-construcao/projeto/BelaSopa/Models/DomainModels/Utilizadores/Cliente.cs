27964f26c5fe406aba25f91abf5d53ea37273875
Alberto Faria <albertofaria.dev@gmail.com>
2019-06-01 22:18:59 +0100

    Informative commit message.

--- 3-construcao/projeto/BelaSopa/Models/DomainModels/Utilizadores/Cliente.cs
+++ 3-construcao/projeto/BelaSopa/Models/DomainModels/Utilizadores/Cliente.cs
@@ -4,19 +4,21 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BelaSopa.Models.DomainModels.Utilizadores{
-    public class Cliente : Utilizador{
+namespace BelaSopa.Models.DomainModels.Utilizadores
+{
+    public class Cliente : Utilizador
+    {
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [NotMapped, JsonIgnore]
-        public virtual ICollection<ClienteEmentaSemanal> ClienteEmentaSemanal { get; set; }
+        public virtual ICollection<ClienteEmentaSemanal> ClienteEmentaSemanal { get; set; } = new List<ClienteEmentaSemanal>();
         [NotMapped, JsonIgnore]
-        public virtual ICollection<ClienteFavorito> ClienteFavorito { set; get; }
+        public virtual ICollection<ClienteFavorito> ClienteFavorito { set; get; } = new List<ClienteFavorito>();
         [NotMapped, JsonIgnore]
-        public virtual ICollection<ClienteFinalizado> ClienteFinalizado { set; get; }
+        public virtual ICollection<ClienteFinalizado> ClienteFinalizado { set; get; } = new List<ClienteFinalizado>();
 
         [NotMapped, JsonIgnore]
-        public virtual ICollection<ClienteExcluiIngrediente> ClienteExcluiIngrediente { set; get; }
+        public virtual ICollection<ClienteExcluiIngrediente> ClienteExcluiIngrediente { set; get; } = new List<ClienteExcluiIngrediente>();
     }
 }
