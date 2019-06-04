2c7171b20f53d9622f13c5bc17d816614a1ef6e0
Alberto Faria <albertofaria.dev@gmail.com>
2019-06-04 14:37:39 +0100

    Informative commit message.

--- 3-construcao/projeto/BelaSopa/Models/DomainModels/Assistente/Dificuldade.cs
+++ 3-construcao/projeto/BelaSopa/Models/DomainModels/Assistente/Dificuldade.cs
@@ -6,4 +6,19 @@ namespace BelaSopa.Models.DomainModels.Assistente
         Media,
         Dificil
     }
+
+    public static class DificuldadeMetodos
+    {
+        public static string GetNome(this Dificuldade dificuldade)
+        {
+            switch (dificuldade)
+            {
+                case Dificuldade.Facil: return "Fácil";
+                case Dificuldade.Media: return "Média";
+                case Dificuldade.Dificil: return "Difícil";
+
+                default: return null;
+            }
+        }
+    }
 }
